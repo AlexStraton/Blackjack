7913f470550fcbc5ddd762ccbf6b3f022f775ef1
AlexStraton <alexandra.straton@curaleafint.com>
2025-01-12 14:13:37 +0000

    refactor: added art image and spaces between the lines

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,5 @@
 from src.deck import Deck
-
+from src.art import logo
 
 def play():
     deck = Deck()
@@ -7,54 +7,51 @@ def play():
     player_cards = []
     computer_cards = []
 
+    print(logo)
     deck.hand_card(player, player_cards, computer_cards)
     deck.hand_card(player, player_cards, computer_cards)
 
     player = False
     deck.hand_card(player, player_cards, computer_cards)
 
-    initial_q = input("Do you want to play Blackjack? Type 'y' or 'n': ")
-    print(initial_q)
+    initial_q = input("Do you want to play Blackjack? Type 'y' or 'n': " + "\n")
 
     if initial_q.lower() == 'y':
         while True:
             total_player = sum(player_cards)
             total_computer = sum(computer_cards)
-            print(f"Your cards: {player_cards}, current score: {total_player}")
-            print(f"Computer's first card: {computer_cards}")
+            print(f"Your cards: {player_cards}, current score: {total_player}" + "\n")
+            print(f"Computer's first card: {computer_cards}" + "\n")
             player = True
 
             if total_player == 21:
-                print("You got 21! You WON!!!!")
+                print("YOU GOT 21! YOU WON!!!!" + "\n")
                 break
             if total_player > 21:
-                print("You went over 21! You lost!")
+                print("YOU WENT OVER 21! YOU LOST!" + "\n")
                 break
 
-            another_card = input("Type 'hit' to HIT, or 'stand' to STAND: ")
-            print(another_card)
+            another_card = input("Type 'hit' to HIT, or 'stand' to STAND: " + "\n")
 
             if another_card.lower() == 'hit':
-                #player continues drawing
                 deck.hand_card(player, player_cards, computer_cards)
                 if total_player == 21:
-                    print("You got 21! You WON!!!!")
+                    print("YOU GOT 21! You WON!!!!" + "\n")
                     break
             else:
-                #computer plays
                 player = False
                 while total_computer < 17:
                     deck.hand_card(player, player_cards, computer_cards)
                     total_computer = sum(computer_cards)
                     if total_computer == 21:
-                        print("Computer got 21! You LOST!!!!")
+                        print("COMPUTER 21! YOU LOST!!!!" + "\n")
                         break
 
-                print(f"Your final cards: {player_cards}, final score: {total_player}")
-                print(f"Computer's final hand: {computer_cards}, final score: {total_computer}")
+                print(f"Your final cards: {player_cards}, final score: {total_player}" + "\n")
+                print(f"Computer's final hand: {computer_cards}, final score: {total_computer}" + "\n")
 
                 if total_computer > 21:
-                    print("Computer went over 21! You WON!")
+                    print("COMPUTER WENT OVER 21! YOU WON!" + "\n")
                     break
 
                 deck.check_win(player_cards, computer_cards)
